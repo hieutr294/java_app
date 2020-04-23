2d9907b4ad7d40525b9680b8a59fc716a1694d6f
hieu <minhhieutruong226@gmail.com>
2020-04-23 18:48:11 +0700

    first commit

---
+++ src/sample/MyApp.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MyApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello");
+        stage.show();
+    }
+}
